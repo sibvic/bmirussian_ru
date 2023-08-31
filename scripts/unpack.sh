83197486a464225d73a88be96241ac7d2bc32cdb
Victor Tereschenko <sibvic@gmail.com>
2023-08-31 10:34:37 +0600

    deploy: stop/start service after update

--- scripts/unpack.sh
+++ scripts/unpack.sh
@@ -1,5 +1,7 @@
 unzip bmi.zip -d ~/to_deploy
 rm ~/to_deploy/appsettings.json
 rm bmi.zip
+systemctl stop bmirussian_ru.service
 cp -r ~/to_deploy/* /var/bmirussian_ru
+systemctl start bmirussian_ru.service
 rm -r ~/to_deploy

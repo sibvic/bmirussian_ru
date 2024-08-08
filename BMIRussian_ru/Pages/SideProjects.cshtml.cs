cd85e0ee2f388a4dc2dc31562d85dea230ef29b8
Victor Tereschenko <sibvic@gmail.com>
2024-08-08 09:49:58 +0600

    [feat] Side projects page

---
+++ BMIRussian_ru/Pages/SideProjects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BMIRussian_ru.Pages
+{
+    public class SideProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

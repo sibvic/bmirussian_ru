e1d2da8990eb435bc39a84f78e924d839e029b7b
Victor Tereschenko <sibvic@gmail.com>
2025-12-02 09:14:00 +0600

    feat: check for zip exists + clean logs

--- scripts/unpack.sh
+++ scripts/unpack.sh
@@ -1,3 +1,9 @@
+if [ ! -f bmi.zip ]; then
+    echo "Error: bmi.zip does not exist"
+    exit 1
+fi
+
+journalctl --vacuum-size=10M
 unzip bmi.zip -d ~/to_deploy
 rm ~/to_deploy/appsettings.json
 rm bmi.zip
